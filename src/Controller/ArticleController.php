c4cc895ed03f9432d79b14d6be4749bc865c1f1a
nicaux <nicaux95@gmail.com>
2018-12-05 09:56:17 +0100

    3e commit

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,6 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
  * Class ArticleController
  * @package App\Controller
  * @Route("/article")
+ *
  */
 class ArticleController extends AbstractController
 {
